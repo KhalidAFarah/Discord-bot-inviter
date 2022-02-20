0db499e8ddff91fc136282117b5bbea06539122e
KhalidAFarah <56063705+KhalidAFarah@users.noreply.github.com>
2022-02-20 16:08:21 +0100

    user now gets the roles when inputing code in just the intvite text channel.
    - The bot now only works in the invite text channel.
    - The bot also gives the user the "Regular" role.
    - Bot also gives a descriptive message.

--- src/main/java/com/example/inviter/discord/DiscordEvents.java
+++ src/main/java/com/example/inviter/discord/DiscordEvents.java
@@ -1,5 +1,6 @@
 package com.example.inviter.discord;
 
+import net.dv8tion.jda.api.entities.Role;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 
@@ -14,10 +15,23 @@ public class DiscordEvents extends ListenerAdapter {
     }
 
     public void onMessageReceived(MessageReceivedEvent event){
-        for(int i = 0; i < codes.size(); i++){
-            if(codes.get(i).equals(event.getMessage().getContentDisplay())){
-                codes.remove(i);
-                event.getTextChannel().sendMessage("correct code").queue();
+        //check channel
+        if(event.getTextChannel().getName().equals("invite")) {
+            for (int i = 0; i < codes.size(); i++) {
+                if (codes.get(i).equals(event.getMessage().getContentDisplay())) {
+                    codes.remove(i);
+                    event.getTextChannel().sendMessage("Hello and welcome to "+ event.getGuild().getName() + ".\n You've been granted access to the server").queue();
+
+
+                    //give user Regular role
+                    Role role = event.getGuild().getRolesByName("Regular", false).get(0);
+                    event.getGuild().addRoleToMember(event.getMember(), role).queue();
+
+                    if(codes.size() == 0){
+                        CodeGenerator.fillList(codes, 50);
+                    }
+                    return;
+                }
             }
         }
 
